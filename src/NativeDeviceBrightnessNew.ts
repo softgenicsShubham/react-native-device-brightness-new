ccc04afba3b7fabd8680a9bbce785103c285105c
Shubham ghanghotia <Softgenics.shubham@gmail.com>
2024-10-16 19:28:34 +0530

    chore: bump version to 0.1.2

--- src/NativeDeviceBrightnessNew.ts
+++ src/NativeDeviceBrightnessNew.ts
@@ -1,8 +1,26 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
+// Define the interface for your TurboModule
 export interface Spec extends TurboModule {
-  multiply(a: number, b: number): number;
+  /**
+   * Method to get the current screen brightness
+   * @returns A promise that resolves to a number between 0.0 and 1.0, representing the brightness level.
+   */
+  getBrightness(): Promise<number>;
+
+  /**
+   * Method to set the screen brightness
+   * @param brightness A number between 0.0 and 1.0 to set the brightness level.
+   * @returns A promise that resolves when the brightness is set.
+   */
+  setBrightness(brightness: number): Promise<void>;
+
+  /**
+   * Method to reset the brightness to the system default.
+   * @returns A promise that resolves when the brightness is reset.
+   */
+  resetBrightness(): Promise<void>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('DeviceBrightnessNew');
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 const DeviceBrightnessNew = require('./NativeDeviceBrightnessNew').default;
 
-export function multiply(a: number, b: number): number {
-  return DeviceBrightnessNew.multiply(a, b);
+export function getBrightness(): Promise<number> {
+  return DeviceBrightnessNew.getBrightness();
+}
+
+export function setBrightness(brightness: number): Promise<void> {
+  return DeviceBrightnessNew.setBrightness(brightness);
+}
+
+export function resetBrightness(): Promise<void> {
+  return DeviceBrightnessNew.resetBrightness();
 }
