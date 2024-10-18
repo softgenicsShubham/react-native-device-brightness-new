d97711b475fafabe5a7dbc8680b0d55e680fc093
Shubham ghanghotia <Softgenics.shubham@gmail.com>
2024-10-18 10:33:31 +0530

    chore(ios): enable device brightness support for ios

--- ios/DeviceBrightnessNew.mm
+++ ios/DeviceBrightnessNew.mm
@@ -1,54 +1,58 @@
 #import "DeviceBrightnessNew.h"
 #import <UIKit/UIKit.h>
 
+
+
 @implementation DeviceBrightnessNew
 RCT_EXPORT_MODULE()
 
-// Don't compile this code when we build for the old architecture.
+
 #ifdef RCT_NEW_ARCH_ENABLED
-
-// Method to get the current screen brightness
-- (void)getBrightness:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
-    @try {
-        CGFloat brightness = [UIScreen mainScreen].brightness;  // Get the current brightness level
-        resolve(@(brightness));  // Return brightness as a number between 0.0 and 1.0
-    } @catch (NSException *exception) {
-        reject(@"E_BRIGHTNESS_ERROR", @"Failed to get brightness", nil);
-    }
+- (void)getBrightness:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+  @try {
+      dispatch_async(dispatch_get_main_queue(), ^{
+          CGFloat brightness = [UIScreen mainScreen].brightness;
+          resolve(@(brightness));
+      });
+  } @catch (NSException *exception) {
+      reject(@"E_BRIGHTNESS_ERROR", @"Failed to get brightness", nil);
+  }
 }
 
-// Method to set the screen brightness
-- (void)setBrightness:(double)brightness resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
-    @try {
-        if (brightness < 0.0 || brightness > 1.0) {
-            reject(@"E_INVALID_BRIGHTNESS_VALUE", @"Brightness value should be between 0.0 and 1.0", nil);
-            return;
-        }
-        [[UIScreen mainScreen] setBrightness:brightness];  // Set the screen brightness
-        resolve(@YES);  // Return success
-    } @catch (NSException *exception) {
-        reject(@"E_SET_BRIGHTNESS_ERROR", @"Failed to set brightness", nil);
-    }
+- (void)resetBrightness:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+   @try {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CGFloat systemBrightness = 0.5;
+        [[UIScreen mainScreen] setBrightness:systemBrightness];
+        resolve(@YES);
+    });
+  } @catch (NSException *exception) {
+    reject(@"E_RESET_BRIGHTNESS_ERROR", @"Failed to reset brightness", nil);
+  }
 }
 
-// Method to reset the brightness to system default
-- (void)resetBrightness:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+- (void)setBrightness:(double)brightness resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
     @try {
-        CGFloat systemBrightness = 0.5;  // Assuming system default is 0.5, adjust based on your logic
-        [[UIScreen mainScreen] setBrightness:systemBrightness];  // Reset the brightness
-        resolve(@YES);  // Return success
-    } @catch (NSException *exception) {
-        reject(@"E_RESET_BRIGHTNESS_ERROR", @"Failed to reset brightness", nil);
-    }
+      if (brightness < 0.0 || brightness > 1.0) {
+          reject(@"E_INVALID_BRIGHTNESS_VALUE", @"Brightness value should be between 0.0 and 1.0", nil);
+          return;
+      }
+
+      dispatch_async(dispatch_get_main_queue(), ^{
+          [[UIScreen mainScreen] setBrightness:brightness];
+          resolve(@YES);
+      });
+
+   } @catch (NSException *exception) {
+      reject(@"E_SET_BRIGHTNESS_ERROR", @"Failed to set brightness", nil);
+   }
 }
 
-// TurboModule initialization
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params {
     return std::make_shared<facebook::react::NativeDeviceBrightnessNewSpecJSI>(params);
 }
-
 #endif
+
 
 @end
